0965972cc10aa96d14e70848330475bc505850fb
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-11-13 04:41:15 +0900

    design : reset.css 및 기본 스타일 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import "./assets/reset.css";
+import "./assets/common.css";
 
 const app = createApp(App);
 
